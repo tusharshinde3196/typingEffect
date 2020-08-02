b35d92cac25c210eddc634df8d808b084804dd82
tusharshinde3196 <66311763+tusharshinde3196@users.noreply.github.com>
2020-08-02 08:45:56 +0530

    Add files via upload

---
+++ typing Effect/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var typed=$(".typed");
+
+    $(function(){
+        typed.typed({
+            strings:["Tushar Shinde","Designer","Developer","Freelancer"],
+            typeSpeed:100,
+            loop:true,
+        });
+    });
+})
